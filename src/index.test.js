a6080523ccbb903eea867ae5e0d2f57ace8a41d6
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-07 21:45:54 -0500

    Write test for reverseString function

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./index.js";
 test("first letter of string is capitalized", () => {
   expect(capitalize("string")).toBe("String");
 });
+
+test("string is reversed", () => {
+  expect(reverseString("string")).toBe("gnirts");
+});
