6db8c11cb9e3ba1a3f5cb584b713ac3b1ef2c1f8
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-08 00:21:40 -0500

    Write code for analyzeArray function

--- src/index.js
+++ src/index.js
@@ -70,3 +70,42 @@ function setShiftedAlphabet(shift) {
   });
   return shiftedAlphabet;
 }
+
+export function analyzeArray(arr) {
+  let dataObj = {};
+  dataObj.average = getArrayAverage(arr);
+  dataObj.min = getArrayMin(arr);
+  dataObj.max = getArrayMax(arr);
+  dataObj.length = arr.length;
+  return dataObj;
+}
+
+function getArrayAverage(arr) {
+  return (
+    arr.reduce((acc, num) => {
+      return (acc += num);
+    }, 0) / arr.length
+  );
+}
+
+function getArrayMin(arr) {
+  let len = arr.length;
+  let min = Infinity;
+  while (len--) {
+    if (arr[len] < min) {
+      min = arr[len];
+    }
+  }
+  return min;
+}
+
+function getArrayMax(arr) {
+  let len = arr.length;
+  let max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
+}
