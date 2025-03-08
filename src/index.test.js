a11dff30bf8abd390f0b004eab1a374bc3a12194
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-07 21:56:12 -0500

    Write tests for calculator object

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./index.js";
+import { capitalize, reverseString, calculator } from "./index.js";
 
 test("first letter of string is capitalized", () => {
   expect(capitalize("string")).toBe("String");
@@ -6,4 +6,20 @@ test("first letter of string is capitalized", () => {
 
 test("string is reversed", () => {
   expect(reverseString("string")).toBe("gnirts");
+});
+
+test("calculator can add 2 + 3", () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
+
+test("calculator can subtract 9 - 4", () => {
+  expect(calculator.subtract(9, 4)).toBe(5);
+});
+
+test("calculator can multiply 2 x 3", () => {
+  expect(calculator.multiply(4, 3)).toBe(12);
+});
+
+test("calculator can divide 12 / 3", () => {
+  expect(calculator.divide(12, 3)).toBe(4);
 });
